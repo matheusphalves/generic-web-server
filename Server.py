a11731c0d0a5d7d6477c0c05b874fce30dc8e18b
Matheus Phelipe <matheusphalves@gmail.com>
2022-02-27 18:03:06 -0300

    adicionando mais informações

--- Server.py
+++ Server.py
@@ -4,7 +4,19 @@ from ServerWeb import Client
 
 
 class Server:
-    def __init__(self, port = 5001, address = '', max_connections = 5) -> None:
+    """A class that represents a Server listening your Clients.
+        Attributes
+    ----------
+    port : int
+        represents the port used to bind. The default value is 5001
+    address : str
+        represents the address used to make requests. The default value is http://localhost
+
+    max_connections : int
+        represents the number of the max simultaneous connections on server
+    """
+
+    def __init__(self, port = 5001, address = 'localhost', max_connections = 5) -> None:
         self.port = port
         self.max_connections = max_connections
         self.address = address
@@ -18,14 +30,16 @@ class Server:
             self.server_socket.listen(self.max_connections)
 
         except Exception as ex:
-            raise f'Error during server initialization: {ex}' 
+            print(f'Error during server initialization: {ex}') 
 
     def start_server(self):
         print(f'Starting server on: {self.address}:{self.port}')
         start_time = datetime.now()
         while self.__isAlive:
-            connection, adress  = self.server_socket.accept() #retorna socket de conexão realizado e endereço do usuário
-            new_process = Client(connection, adress) #creating new instance of Client class
+            connection, address  = self.server_socket.accept() #return socket and user address
+            print(address)
+            print(connection)
+            new_process = Client(connection, address) #creating new instance of Client class
             new_process.start() #start proccess with conection received
         end_time = datetime.now()
         print(f'Finished execution. Duration: {end_time - start_time}')
--- ServerWeb.py
+++ ServerWeb.py
@@ -1,65 +1,69 @@
-import socket
 from threading import Thread
 from utils.ResponseBuilder import response_builder
 
-#******************************CLASSE DE THREAD COM CLIENTE
-class Client(Thread): #cada instância dessa classe possui um processo em execução para um único cliente
-    #herança da classe Thread
-    def __init__(self,socket_client,ip_Adress):   #método construtor da classe
+class Client(Thread):
+    """A class that represents a Client conection, inheriting a thread of control.
+    To execute this class, was necessary to overring the run() method.
+    """
+
+    def __init__(self,socket_client,ip_Adress):
         Thread.__init__(self)
         self.lista = ["GET", "HEAD"]
         self.socket_client = socket_client
         self.ip_Adress = ip_Adress 
 
-    def run(self):#método é executado ao fazermos Thread.start()
+    def run(self):#called when type Thread.start()
         self.socket_client.setblocking(True)
         while True:
-            try: #caso o socket esteja aberto
+            try:
                 msg = self.socket_client.recv(2000)  
             except:
                 break
             data = msg.decode().split(" ")
-            resposta , arq = self.response(data) #recebe corpo response e arquivo requisitado
+            print(data[0])
+            resposta , arq = self.response(data) #retrives response body and resource
             self.socket_client.sendall(resposta.encode())
             if(arq!=None):
                 self.socket_client.sendall(arq)       
             self.socket_client.close()
             
     def response(self,dado):
-        if(dado[0] in self.lista): #se a string estiver no array
+        if(dado[0] in self.lista): #it's a allowed method
             if(dado[0]=="GET"):
-                return self.get(dado[1:]) #lista a partir do índice 1 em diante
+                return self.get(dado[1:]) #handle GET
             elif(dado[0]=="HEAD"):
-                return self.head(dado[1:],False) #False: nnao quero receber o arquivo
+                return self.head(dado[1:],False) #handle HEAD
 
         return (response_builder('HTTP', '1.1', '405', 'Method Not Allowed'), None)
 
     def get(self, dado):
-        response, readFile = self.head(dado, True) #recebe cabeçalho do response e arquivo requisitado.
-                                                    #True: quero receber arquivo se ele existir
-        if(readFile!=None): #verifica se path contém / no início (fatiamento de string)
-            diretorio = str.rpartition(dado[0], ".")#cria lista com diretório do arquivo e extensão
+        response, readFile = self.head(dado, True) #retrives request headers and requested resource
+                                                    #True: that's means the server needs return the requested file
+        
+        if(readFile!=None): #check if path contains '/' on start
+            diretorio = str.rpartition(dado[0], ".")#create list with path to file
             print("[Arquivo solicitado:", dado[0], "]")
             if(diretorio[2].find("html")==-1):
-                return (response, readFile) #envio de response e arquivo solicitado     
+                return (response, readFile) #send response and requested file     
             else:
                 response = response + readFile.decode("utf-8") 
-                return (response, None) #página enviada no corpo response
+                return (response, None) #send page in response body
         
         return (response_builder('HTTP', '1.1', '404', 'File not found'), None)
 
 
+    
     def head(self, dado, flag):
-        #método retorna cabeçalho de requisição e arquivo solicitado
-        directory = str.rpartition(dado[0], ".")#cria lista com diretório do arquivo e extensão
-        if(dado[0][0]=="/"): #verifica se path contém / no início (fatiamento de string)
-            if(len(dado[0])==1):#retornar index.html da raiz do servidor
+        #returns headers and resource of request call
+        directory = str.rpartition(dado[0], ".") #create list with path to file
+        if(dado[0][0]=="/"): #check if path contains '/' on start
+            if(len(dado[0])==1):#returns index.html from root path
                 try:
-                    read_file = open('index.html', 'rb').read() #leitura como texto
+                    read_file = open('index.html', 'rb').read()
                 except:
                     return (response_builder('HTTP', '1.1', '404', 'File not found'), None)
             else: 
-                try:#leitura de um arquivo qualquer
+                try:
                     directory = str.rpartition(dado[0], ".")
                     read_file = open(directory[0][1:] + "." + directory[2], 'rb').read()
                 except:
